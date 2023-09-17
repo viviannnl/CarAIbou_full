0c419e20ae9c43bd780abcf72e55700aabbfb72a
Vivian Li <viviannnlyr23@gmail.com>
2023-09-16 21:47:26 -0400

    deployment prepare

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 //const { use } = require('./routes/userRoutes')
 const dotenv = require('dotenv').config()
@@ -13,5 +14,12 @@ app.use(express.urlencoded({extended: false}));
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/habits', require('./routes/habitsRoutes'))
 app.use('/api/goals', require('./routes/goalsRoutes'))
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
