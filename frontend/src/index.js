144c14a4f7ba41053e1ebb76ea94ca4ddf1c3b18
Vivian Li <viviannnlyr23@gmail.com>
2023-09-21 20:17:30 -0400

    changes for deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,9 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const container = document.getElementById('root');
 const root = createRoot(container);
